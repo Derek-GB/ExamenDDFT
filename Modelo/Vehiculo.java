727fb8a35bee6aca94d4c658ea49638829a20613
Derek-GB <degonzalezba@est.utn.ac.cr>
2024-10-23 13:57:05 -0600

    Creacion de clase Vehiculo
    Clase tonta del modelo, con sus gets y metodo constructor

---
+++ Modelo/Vehiculo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Student
+ */
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private String matricula;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public Vehiculo(String marca, String modelo, String matricula) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+    }
+}
