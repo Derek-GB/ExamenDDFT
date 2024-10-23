348a8f018b88abc36aff4e02547c08acd5159d33
AnthonyC <Twonejo@gmail.com>
2024-10-23 15:16:43 -0600

    se creo la clase GestorMantenimiento la anterior iba vacia(no guarde)
    se creo la clase con sus metodos add,update,consultar ademas de la implementacion del patron singleton

--- Modelo/GestorMantenimiento.java
+++ Modelo/GestorMantenimiento.java
@@ -4,10 +4,52 @@
  */
 package Modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Student
  */
 public class GestorMantenimiento {
+
+    private static GestorMantenimiento mantenimiento;
+    ArrayList<Vehiculo> lista = new ArrayList();
+    ServicioMantenimiento servicio;
+    Vehiculo vehiculo;
     
+
+    public void agregar(Vehiculo vehiculo) {
+        lista.add(vehiculo);
+    }
+
+    public void actualizar(Vehiculo vehiculo) {
+        if (lista.contains(vehiculo.getMatricula())) {
+            Vehiculo v = new Vehiculo(vehiculo.getMarca(), vehiculo.getModelo(), vehiculo.getMatricula());
+            lista.remove(vehiculo);
+            lista.add(v);
+        }
+
+    }
+
+    public void consultarEstado(Vehiculo vehiculo) {
+        servicio.getEstado();
+    }
+    
+ 
+
+    private GestorMantenimiento() {
+        mantenimiento = new GestorMantenimiento();
+    }
+
+    public static GestorMantenimiento getInstance() {
+        if (mantenimiento == null) {
+            mantenimiento = new GestorMantenimiento();
+            return mantenimiento;
+        }
+        return mantenimiento;
+    }
+
+    public void nose(){
+        System.out.println("nose");
+    }
 }
