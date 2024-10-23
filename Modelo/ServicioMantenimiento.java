8dead9f64b524e38fb5952ae586a91f53f22616b
fer <febolivarbr@est.utn.ac.cr>
2024-10-23 13:59:18 -0600

    Creación clase ServicioMantenimiento
    Falta aplicar el Enum y State

---
+++ Modelo/ServicioMantenimiento.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Student
+ */
+public class ServicioMantenimiento {
+    
+    private String tipo;
+    private String detalles;
+    private Estado estado; 
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(String detalles) {
+        this.detalles = detalles;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public ServicioMantenimiento(String tipo, String detalles, Estado estado) {
+        this.tipo = tipo;
+        this.detalles = detalles;
+        this.estado = estado;
+    }
+    
+      public ServicioMantenimiento() {
+        this("","",Estado.espera);
+    }
+
+    @Override
+    public String toString() {
+        return "ServicioMantenimiento{" + "tipo=" + tipo + ", detalles=" + detalles + ", estado=" + estado + '}';
+    }
+    
+    
+    
+}
